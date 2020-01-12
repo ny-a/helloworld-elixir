215886c775cd65ce33e3278cc2fc03d2cc91dcbd
ny-a <58758248+ny-a@users.noreply.github.com>
2020-01-13 01:24:21 +0900

    Initial commit

---
+++ lib/helloworld_elixir.ex
@@ -0,0 +1,18 @@
+defmodule HelloworldElixir do
+  @moduledoc """
+  Documentation for HelloworldElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloworldElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
